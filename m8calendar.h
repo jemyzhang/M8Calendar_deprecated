3ba05ffd27495b222dfc4ab933fb9b3722790940
jemyzhang <jemyzhang@0e50ebb1-d170-7b45-893d-78c2c9fc94b7>
2009-04-23 08:45:31 +0000

    1. 使用sqlite静态库链接，减少代码编译时间 2. 使用upx压缩exe，大大减小了exe的大小
    git-svn-id: https://172.16.185.65/svn/M8Calendar/trunk@5 0e50ebb1-d170-7b45-893d-78c2c9fc94b7

--- m8calendar.h
+++ m8calendar.h
@@ -1,7 +1,7 @@
 #include "ui_calendar.h"
 
-#define VER_STRING L"1.1"
-#define BUILD_STRING L"20090421"
+#define VER_STRING L"1.2"
+#define BUILD_STRING L"20090423"
 #define APPNAME L"уфиое╘юЗ"
 
 // Application class derived from CMzApp
